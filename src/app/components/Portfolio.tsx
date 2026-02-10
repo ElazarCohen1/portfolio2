616fc44187668cf1f21fb30c8bfcae8e6fbf36b9
elazar <elazacohen01@gmail.com>
2026-02-10 09:50:19 +0100

    add skills and change cv

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -4,46 +4,56 @@ import gan_eden from '@/public/project/gan_eden.png'
 import portfolio from '@/public/project/portfolio.png'
 import Layer_website from '@/public/project/Layer_website.png'
 import villa_alerces from '@/public/project/villa_alerces.png'
+import codec from '@/public/project/codec.png'
 function Portfolio(){
     return (
         <div id="portfolio">
-            <h1 id="title_portfolio">
-                Portfolio
-            </h1>
+            <h1 id="title_portfolio">Portfolio</h1>
+
             <div id="projects">
                 <div id="grid_project">
-                    <Project
-                     name= "Showcase site for a lawyer"
-                     imageUrl={Layer_website}
-                     href= "https://elazarcohen1.github.io/site-cabinet-cohen/#/"
-                     description="A clean and professional showcase website designed for a lawyer, highlighting services, expertise, and contact information."
-                    langages={['react','Css']} 
 
-                    />
-                    
                     <Project
-                        name= "ShowCase Website for a customer"
+                        name="Showcase site for a lawyer"
+                        imageUrl={Layer_website}
+                        href="https://elazarcohen1.github.io/site-cabinet-cohen/#/"
+                        description="A professional and elegant showcase website for a law firm, presenting legal services, areas of expertise, and clear contact information."
+                        langages={['react','Css']} 
+                    />
+
+                    <Project
+                        name="Website for villa reservation"
                         imageUrl={villa_alerces}
-                        href= "https://villa-alerces.vercel.app/"
-                        description="A clean and professional showcase website designed for a lawyer, highlighting services, expertise, and contact information."
+                        href="https://villa-alerces.vercel.app/"
+                        description="A villa reservation website featuring property details, availability information, and a smooth user experience for potential guests."
                         langages={['react','Css']} 
-
                     />
+
                     <Project
-                        name= "ShowCase Website for a customer"
+                        name="e-commerce Website for a flower shop"
                         imageUrl={gan_eden}
-                        href= "https://dreadful-impact-back.medusajs.site/fr"
-                        description="A clean and professional showcase website designed for a lawyer, highlighting services, expertise, and contact information."
-                        langages={['react','Css']} 
-
+                        href="https://dreadful-impact-back.medusajs.site/fr"
+                        description="An e-commerce platform for a flower shop, allowing users to browse products, manage orders, and purchase floral arrangements online."
+                        langages={['nextjs','tailwind']} 
                     />
 
-                    <Project name="My portfolio"
-                        imageUrl={portfolio}   
-                        langages={['react','Css']} 
-                        projet="portfolio" 
-                        description="
-                        this site web is made in react with css and this is my portfolio"/>
+                    <Project
+                        name="My portfolio"
+                        imageUrl={portfolio}
+                        href="https://portfolio2-eta-five-81.vercel.app/"
+                        description="My personal portfolio website showcasing my projects, skills, and technologies I work with as a web developer."
+                        langages={['nextjs','react','css']} 
+                        projet="portfolio2"
+                    />
+
+                    <Project
+                        name="Projet en c"
+                        imageUrl={codec}
+                        description="Implementation of a custom codec written in C, focusing on low-level data encoding and decoding mechanisms. The project emphasizes performance, memory management, and modular design, and is built using Makefiles with shared libraries (.so)."
+                        langages={['C','Makefile','libso']} 
+                        projet="Codec-c"
+                    />
+
                 </div>
             </div>
         </div>
--- src/app/components/skill_menu.tsx
+++ src/app/components/skill_menu.tsx
@@ -6,12 +6,20 @@ import python_logo from '@/public/langage/python.png'
 import java_logo from '@/public/langage/java.png'
 import php_logo from '@/public/langage/php.png'
 import c_logo from '@/public/langage/C.png'
+import haskell_logo from '@/public/langage/haskell.png'
+import nasm_logo from '@/public/langage/nasm.png'
+import bash_logo from '@/public/langage/bash.png'
+
 
 import tailwind_logo from '@/public/langage/tailwind.png'
 import html_logo from '@/public/langage/html.png'
 import css_logo from '@/public/langage/css.png'
 import flutter_logo from '@/public/langage/flutter.png'
 import react_logo from'@/public/langage/react.png' 
+import sql_logo from '@/public/langage/sql.png' 
+import nextjs_logo from '@/public/langage/nextjs.png'
+import typescript_logo from '@/public/langage/typescript.png'
+
 import git_logo from '@/public/langage/git.png'
 import github_logo from '@/public/langage/github.png'
 import vscode_logo from '@/public/langage/vscode.png'
@@ -26,14 +34,23 @@ const SkillsMenu = () => {
     { name: 'Java', imageUrl:  java_logo},
     { name: 'PHP', imageUrl:  php_logo},
     { name: 'C', imageUrl: c_logo },
+    {name: "Haskell",imageUrl:haskell_logo},
+    {name: "Nasm",imageUrl:nasm_logo},
+    {name: "Bash",imageUrl:bash_logo},
+
+
   ];
 
   const skillsWeb = [
     { name: 'HTML', imageUrl:  html_logo},
     { name: 'CSS', imageUrl:  css_logo},
+    {name : 'SQL',imageUrl:sql_logo },
     { name: 'Flutter', imageUrl: flutter_logo },
     { name: 'React', imageUrl: react_logo},
     { name: 'Tailwind', imageUrl: tailwind_logo},
+    {name:"Nextjs", imageUrl: nextjs_logo},
+    {name:"TypeScript", imageUrl: typescript_logo}
+
 
   ];
 
